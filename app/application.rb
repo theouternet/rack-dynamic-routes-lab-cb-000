9a5ddea70bd3d72012677e435a21d0f958c5b584
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-03-15 23:17:08 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,7 @@ class Application
       item_name = req.path.split("/items/").last
       
       if @@items.find {|i| i.name == item_name}
-        resp.write item_name.price
+        resp.write self.price
       else 
       resp.write "Item not found"
       resp.status = 400
