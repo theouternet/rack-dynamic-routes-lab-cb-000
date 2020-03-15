5fb728e7646ca6eee8fe127bfbaf2d4344149789
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-03-15 22:48:03 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,25 @@
+class Application
+  
+  @@items = []
+ 
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+ 
+ 
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      
+      if @@items.detect(item_name)
+        resp.write item_name.price
+      end 
+      
+      resp.write "You requested the songs"
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+ 
+    resp.finish
+  end
+end
